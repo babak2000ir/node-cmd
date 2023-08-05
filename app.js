6d8967f9459264578d70f371532dbea4c03cb8da
Babak Jahedmanesh <babak.jahedmanesh@thenavpeople.co.uk>
2023-08-05 19:13:26 +0100

    shell interpreter

--- app.js
+++ app.js
@@ -5,34 +5,38 @@ const bodyParser = require("koa-bodyparser");
 const app = new Koa();
 app.use(bodyParser());
 
-app.use(async (ctx, next) => {
-    const result = await asyncRunInShell();
+app.use(async (ctx) => {
+    const commandString = ctx.request.body.commandString;
+    const result = await asyncRunInShell(commandString);
     ctx.body = result;
 });
 
 app.listen(3000);
 console.log("Server listening on port 3000");
 
-async function asyncRunInShell() {
+async function asyncRunInShell(cmdString) {
     return new Promise((resolve) => {
         let result = "";
-        const cmd = spawn('dir', [], { shell: true });
-    
+
+        const command = cmdString.split(' ')[0];
+        const args = cmdString.split(' ').splice(1);
+        const cmd = spawn(command, args, { shell: true });
+
         cmd.stdout.on('data', (data) => {
             result += data + "\n";
         });
-    
+
         cmd.stderr.on('data', (data) => {
             result += data + "\n";
         });
-    
+
         cmd.stderr.on('error', (err) => {
             result += err + "\n";
         });
-    
+
         cmd.on('close', (code) => {
-            result += `child process exited with code ${code}`;
+            result += `Exit code: ${code}`;
             resolve(result);
         });
     });
-  }
+}
