4a0bd5ea3455a77dc7d6b27d1208bdaec9f30f1a
Babak Jahedmanesh <babak.jahedmanesh@thenavpeople.co.uk>
2023-08-11 21:32:32 +0100

    web terminal - dangerous and open!

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
+import { ReactTerminal } from "react-terminal";
 import './App.css';
-
 function App() {
-  const label = "Ada";
+  async function myHandler() {
+    var args = Array.prototype.slice.call(arguments);
+    const settings = {
+      method: 'POST',
+      body: JSON.stringify({ commandString: args.join(' ') }),
+      headers: {
+        'Content-Type': 'application/json',
+        "accepts": "application/json"
+      }
+    }
+    return await fetch('/api', settings)
+      .then(res => res.text())
+      .then(text => {
+        const responseArr = text.split('\n');
+        return responseArr.map((response, idx) =>
+          <><span key={idx}>{response}</span><br /></>
+        );
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>{label}</p>
+        <div style={{ height: 600, maxHeight: "100vh", width: '90%', maxWidth: "100vw" }}>
+          <ReactTerminal defaultHandler={myHandler} theme="dark" />
+        </div>
       </header>
     </div>
   );
